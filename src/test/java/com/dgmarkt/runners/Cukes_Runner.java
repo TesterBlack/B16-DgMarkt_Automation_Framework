b9d8fd8f45bcc047ed7c002e8504c813e8169d43
peace <mehmetakkaya1990@gmail.com>
2024-09-04 11:02:30 +0300

    added: dryrun = true

--- src/test/java/com/dgmarkt/runners/Cukes_Runner.java
+++ src/test/java/com/dgmarkt/runners/Cukes_Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features="src/test/resources/features",
         glue="com/dgmarkt/step_definitions",
-        dryRun = false,
+        dryRun = true,
         tags = ""
 
 
